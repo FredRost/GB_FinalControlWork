32944d4f94e578129daed5f366c9a488fd45d538
FredRost <fredrost161@gmail.com>
2023-06-12 12:32:37 +0300

    InputArray add

--- Program.cs
+++ Program.cs
@@ -3,3 +3,14 @@
     System.Console.WriteLine(message);
     return Convert.ToInt32(Console.ReadLine());
 }
+
+string[] InpuntArray(int sizeArray)
+{
+    string[] array = new string[sizeArray];
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"Введите {i + 1} элемент массива");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
