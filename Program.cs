ae225581729c96e2abeeddd48d62c966fbdb98de
FredRost <fredrost161@gmail.com>
2023-06-12 12:33:26 +0300

    LessThanThreeArray add

--- Program.cs
+++ Program.cs
@@ -23,3 +23,22 @@ void PrintArray(string[] array)
     }
     System.Console.WriteLine();
 }
+
+string[] LessThanThreeArray(string[] array)
+{
+    string[] resultArray = new string[array.Length];
+
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            {
+                resultArray[j] = array[i];
+                j++;
+            }
+        }
+    }
+
+    return resultArray;
+}
