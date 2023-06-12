0559c4ac3a336ff5b5914baba63e50a0b4c84113
FredRost <fredrost161@gmail.com>
2023-06-12 12:32:00 +0300

    InputInt add

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputInt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
